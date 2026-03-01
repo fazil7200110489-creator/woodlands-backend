3549d148d580725c370f5a45487f6e13f704acb2
fazil7200110489-creator <fazil721655@gmail.com>
2026-03-01 16:32:30 +0530

    Updated socket CORS for production

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 // 🔥 Socket Setup
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT"]
   }
 });
